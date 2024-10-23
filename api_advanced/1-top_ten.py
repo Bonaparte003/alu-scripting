2c52afc62a115328c5242393d2ff2bff47465759
Bonaparte <b.uteramaho@alustudent.com>
2024-10-23 11:56:42 +0200

    task2

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""function that fetches from reddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """function that fetches top 10 hottest posts"""
+    headers = {'User-Agent': 'MyAPI/0.1'}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    fetched_data = requests.get(url, headers=headers, allow_redirects=False)
+    status = fetched_data.status_code
+    if status == 200:
+        results = fetched_data.json()
+        data = results['data']['children']
+        hot_posts = ""
+        for i in data:
+            hot_posts += i['data']['title'] + "\n"
+        print(hot_posts)
+    else:
+        print("None")
