2efe25e328ebf0512a080ac59ca5b1cc6975b3bd
Bonaparte <b.uteramaho@alustudent.com>
2024-10-23 10:14:42 +0200

    task1

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function that fetches data from Reddit API
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that fetches data"""
+
+    headers = {'User-Agent': 'MyAPI/0.1'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    fetched_data = requests.get(url, headers=headers, allow_redirects=False)
+    results = fetched_data.json()
+    status = fetched_data.status_code
+    if status == 200:
+        subscribers = results['data']['subscribers']
+    else:
+        subscribers = 0
+    return subscribers
