21adc718bc0908c4ea5498b8b38f5563d6697dd3
Bonaparte <b.uteramaho@alustudent.com>
2024-10-23 12:55:56 +0200

    trial2

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-the 10 hotest
-"""
+"""function that fetches from reddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    making the function
-    """
+    """"function that fetches data """
     if subreddit is None or type(subreddit) is not str:
         print(None)
     headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-            AppleWebKit/537.36 (KHTML, like Gecko)\
-            Chrome/102.0.0.0 Safari/537.36'
+            'User-Agent': 'MyAPI/0.1'
             }
     r = requests.get(
-            f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+            f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9",
             allow_redirects=False,
             headers=headers
             )
     if r.status_code != 200:
         print(None)
         return
-    for child in r.json()["data"]["children"][1:10]:
+    for child in r.json()["data"]["children"][1:]:
         print(child["data"]["title"])
