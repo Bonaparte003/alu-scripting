88ee66879502bae20564cf67ae61d37bedc76d8a
Bonaparte <b.uteramaho@alustudent.com>
2024-10-23 11:11:07 +0200

    task1

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,19 +3,15 @@
 import requests
 
 
-
 def number_of_subscribers(subreddit):
-  """function that fetches data """
-  headers = {'User-Agent': 'MyAPI/0.1'}
-  url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-  fetched_data = requests.get(url, headers=headers, allow_redirects=False)
-  status = fetched_data.status_code
-  if status == 200:
-    try:
-      results = fetched_data.json()
-      subscribers = results['data']['subscribers']
-    except ValueError:
-      subscribers = 0
-  else:
-       subscribers = 0
-  return subscribers
+    """function that fetches data """
+    headers = {'User-Agent': 'MyAPI/0.1'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    fetched_data = requests.get(url, headers=headers, allow_redirects=False)
+    status = fetched_data.status_code
+    if status == 200:
+        results = fetched_data.json()
+        subscribers = results['data']['subscribers']
+    else:
+        subscribers = 0
+    return subscribers
