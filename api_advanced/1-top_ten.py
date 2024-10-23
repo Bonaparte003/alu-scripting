cbdae00e401c759d40250c908a489a29b1af8cc4
Bonaparte <b.uteramaho@alustudent.com>
2024-10-23 12:25:29 +0200

    updates

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """function that fetches top 10 hottest posts"""
     headers = {'User-Agent': 'MyAPI/0.1'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}.json?limit=10".format(subreddit)
     fetched_data = requests.get(url, headers=headers, allow_redirects=False)
     status = fetched_data.status_code
     if status == 200:
