f3c3084c0548c34b55382cd3f2845c50c5348d08
Bonaparte <b.uteramaho@alustudent.com>
2024-10-23 12:32:27 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,15 +6,13 @@ import requests
 def top_ten(subreddit):
     """function that fetches top 10 hottest posts"""
     headers = {'User-Agent': 'MyAPI/0.1'}
-    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}.json?limit=10".format(subreddit)
     fetched_data = requests.get(url, headers=headers, allow_redirects=False)
     status = fetched_data.status_code
     if status == 200:
         results = fetched_data.json()
+        data = results['data']['children']
         for i in range(10):
-            print(results.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title'))
+            print(data[i]['data']['title'])
     else:
         print(None)
