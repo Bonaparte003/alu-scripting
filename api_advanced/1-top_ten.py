12b883249b6eca040844f5968b32746bbfe94910
Bonaparte <b.uteramaho@alustudent.com>
2024-10-23 13:04:24 +0200

    updates

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,14 +4,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """"function that fetches data """
-    if subreddit is None or type(subreddit) is not str:
+    """function that fetches top 10 hottest posts"""
+    headers = {'User-Agent': 'MyAPI/0.1.1'}
+    url = "https://www.reddit.com/r/{}.json?limit=9".format(subreddit)
+    fetched_data = requests.get(url, headers=headers, allow_redirects=False)
+    status = fetched_data.status_code
+    if status == 200:
+        results = fetched_data.json()
+        data = results['data']['children']
+        for i in range(9):
+            print(data[i]['data']['title'])
+    else:
         print(None)
-    headers = {'User-Agent': 'MyAPI/0.1'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
-    r = requests.get(url, allow_redirects=False, headers=headers)
-    if r.status_code != 200:
-        print(None)
-        return
-    for child in r.json()["data"]["children"][1:]:
-        print(child["data"]["title"])
