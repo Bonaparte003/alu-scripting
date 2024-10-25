d2984b1c1d238808fa2f8fd664d929603c6c725b
Bonaparte <b.uteramaho@alustudent.com>
2024-10-25 12:05:12 +0200

    new

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,18 +4,31 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """Print the titles of the 10 hottest posts on
+    a given subreddit or 'None' if it doesn't exist."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False)
+
     if response.status_code != 200:
-        print("OK")
+        print("None")  # Print "None" if the subreddit does not exist
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    results = response.json().get("data", {}).get("children", [])
+    if not results:
+        print("None")
+        return
+
+    for post in results:
+        title = post.get("data", {}).get("title")
+        if title:
+            print(title)
