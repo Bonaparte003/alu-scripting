78b233afa436bca0d13aae72e203def2d930af12
Bonaparte <b.uteramaho@alustudent.com>
2024-10-25 07:51:15 +0200

    done1

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""function that fetches API reddit in rercurse mode"""
-
+""" hotest recursion"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Recursively queries the Reddit API"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 100, 'after': after}
-    try:
-        response = requests.get(
-            url,
+def recurse(subreddit, hot_list=[], params={}):
+    """ recursion back
+    """
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/102.0.0.0 Safari/537.36'
+            }
+    r = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/hot.json",
+            allow_redirects=False,
             headers=headers,
-            params=params,
-            allow_redirects=False)
-
-        if response.status_code != 200:
-            return None
-        data = response.json().get('data')
-        hot_list += [child['data']['title'] for child in data['children']]
-        after = data.get('after')
-        if after is None:
-            return hot_list
-        else:
-            return recurse(subreddit, hot_list, after)
-    except Exception:
-        return None
+            params=params
+            )
+    if r.status_code != 200:
+        return
+    for child in r.json()["data"]["children"][:-1]:
+        hot_list.append(child["data"]["title"])
+    if r.json()["data"]["after"] is None:
+        return hot_list
+    after_value = r.json()["data"]["after"]
+    return recurse(subreddit, hot_list, {"after": after_value})
