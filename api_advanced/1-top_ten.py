6c649f9f8ea56377b454a14bf625e97d6f37bc1b
Bonaparte003 <b.uteramaho@alustudent.com>
2024-10-25 09:22:50 +0000

    updates

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "MyAPI/0.0.1"
     }
     params = {
         "limit": 10
@@ -17,5 +17,9 @@ def top_ten(subreddit):
     if response.status_code == 404:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    else:
+        try:
+            results = response.json().get("data")
+            [print(c.get("data").get("title")) for c in results.get("children")]
+        except ValueError:
+            print("None")
