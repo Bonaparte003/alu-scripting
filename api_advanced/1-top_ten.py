99aa617cd3612a5fdebb3d49991bb25a4608d020
Bonaparte <b.uteramaho@alustudent.com>
2024-10-25 13:19:24 +0200

    tria6

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -21,7 +21,7 @@ def top_ten(subreddit):
 
     response = get(url, headers=user_agent, params=params)
     if response.status_code != 200:
-        print('OK', end='\b')
+        print('OK', end='', flush=True)
         return
 
     try:
