bc412a851b92c6c7e901a24f0571de433d8d1a3d
Bonaparte <b.uteramaho@alustudent.com>
2024-10-25 13:44:45 +0200

    okay

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,18 +12,13 @@ def top_ten(subreddit):
     """
     The Function that fethces the Reddit API
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
     response = get(url, headers=user_agent, params=params)
     if response.status_code != 200:
-        subprocess.call('alias i=OK')
-        subprocess.call('i')
+        print("None")
         return
 
     try:
